7ea9f96f9860598638767193b296b92ffd2c78c9
John Doe <gabin.dietsch@student.junia.com>
2023-11-10 11:03:26 +0100

    [+] Add 'Add Users' button

--- library-site/src/app/users/page.tsx
+++ library-site/src/app/users/page.tsx
@@ -1,13 +1,28 @@
 ﻿'use client';
 
 import Link from 'next/link';
+import Modal from 'react-modal';
 import React, { FC, useEffect, useState } from 'react';
-import { useUsersProviders } from '@/hooks';
+import { useUsersProviders, createNewUser } from '@/hooks';
 import { Navbar } from '../layout';
 
 const UsersPage: FC = () => {
   const { useListUsers } = useUsersProviders();
   const { users, load: loadUsers } = useListUsers();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      width: '50%',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
 
   useEffect(() => {
     loadUsers();
@@ -17,9 +32,28 @@ const UsersPage: FC = () => {
     document.title = 'Users List';
   }, []);
 
+  const handleAddUser = async (e: React.FormEvent): Promise<void> => {
+    e.preventDefault();
+    const newUser = {
+      id: (users.length + 1).toString(),
+      firstname,
+      lastname,
+    };
+
+    await createNewUser(newUser);
+    setIsModalOpen(false);
+  };
+
   return (
     <main>
       <Navbar />
+      <button
+          type="button"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mb-4"
+          onClick={(): void => setIsModalOpen(true)}
+        >
+          Add Users
+        </button>
       <div className="flex justify-around flex-wrap ">
         {users.map((user) => (
           <div className="w-full max-w-sm border border-gray-200 rounded-lg bg-sky-950 shadow dark:border-gray-700 m-10">
@@ -41,6 +75,55 @@ const UsersPage: FC = () => {
           </div>
         ))}
       </div>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={(): void => setIsModalOpen(false)}
+        contentLabel="Ajouter Auteur"
+        style={customStyles}
+      >
+        <h1 className="flex justify-center text-lg mb-4 text-black text-2xl">
+          Add User
+        </h1>
+        <hr className="mb-2" />
+        <form onSubmit={handleAddUser}>
+          <label htmlFor="firstName" className="block mb-2 text-white">
+            First Name
+            <input
+              id="firstName"
+              type="text"
+              placeholder="First Name"
+              value={firstname}
+              onChange={(e): void => setFirstname(e.target.value)}
+              className="flex mt-1 p-2 w-2/3 border-sky-950 text-black rounded-lg"
+            />
+          </label>
+          <label htmlFor="lastName" className="block mb-2 text-white">
+            Last Name
+            <input
+              type="text"
+              placeholder="Last Name"
+              value={lastname}
+              onChange={(e): void => setLastname(e.target.value)}
+              className="flex mt-1 p-2 w-2/3 border-sky-950 text-black rounded-lg"
+            />
+          </label>
+          <div className="flex justify-between text-white">
+            <button
+              type="submit"
+              className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded"
+            >
+              Add User
+            </button>
+            <button
+              type="button"
+              className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded"
+              onClick={(): void => setIsModalOpen(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </Modal>
     </main>
   );
 };
