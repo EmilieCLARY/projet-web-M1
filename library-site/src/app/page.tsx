db8b3292fa0ca64e5958af63b9519ae89c6c8200
EmilieCLARY <emilie.clary@student.junia.com>
2023-11-10 17:17:04 +0100

    [+] Modification BDD

--- library-site/src/app/page.tsx
+++ library-site/src/app/page.tsx
@@ -42,23 +42,6 @@ const Home: FC = (): ReactElement => {
         <h2 className="text-lg text-sky-950">Welcome to</h2>
         <h1 className="text-5xl font-bold text-sky-950">The Readers Oasis</h1>
 
-        {randomBook && (
-          <div className="flex flex-wrap justify-around items-center">
-            <div
-              key={randomBook.id}
-              className="flex flex-col justify-around items-center bg-white rounded-lg shadow-lg p-6 m-8 min-w-max max-w-xs min-h-max border-2 border-sky-950"
-            >
-              <p className="text-2xl font-bold text-sky-950">{randomBook.name}</p>
-              <p className="text-lg text-sky-950">
-                {randomBook.author.firstName} {randomBook.author.lastName}
-              </p>
-              <p className="text-lg text-sky-950">{randomBook.writtenOn}</p>
-              <p className="text-lg text-sky-950">
-                {randomBook.genres.join(', ')}
-              </p>
-            </div>
-          </div>
-        )}
       </div>
     </main>
   );
